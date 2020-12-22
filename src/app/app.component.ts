417d5baff474295e7c73734fe65a2a80875388ab
shresth7001 <42043261+shresth7001@users.noreply.github.com>
2020-12-22 22:29:16 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'Counter App';
+  count: number = 0;
+
+  handleIncrease = () => {
+    this.count = this.count + 1;
+  };
+  handleDecrease = () => {
+    if (this.count>0){
+    this.count = this.count - 1;
+    }
+    
+  };
+  handleReset = () => {
+    this.count = 0;
+  };
+}
